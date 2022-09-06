d858289438803c0d64ccc328d5216f1422a0e8e6
Leonardo Oliveira <dev@leooliveira.me>
2022-09-05 23:52:59 -0300

    "Copy to clipboard" feature

---
+++ src/components/CodeGrid.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  stickers: Sticker[]
+}>();
+</script>
+
+<template>
+  <ul class="grid grid-cols-4 lg:grid-cols-6 gap-4">
+    <li v-for="sticker in stickers" class="tooltip tooltip-top" :data-tip="sticker.name">
+      <button class="btn w-full">{{sticker.code}}</button>
+    </li>
+  </ul>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
+
 import Heading from '../components/Heading.vue';
 import Flag from '../components/Flag.vue';
+import CodeGrid from '../components/CodeGrid.vue';
 
 import { useStore } from '../store';
-import { computed } from 'vue';
 
 const store = useStore();
 const { userData, allStickers, spares, needed } = storeToRefs(store);
 
 const completionPercentage = computed(() => Math.floor(100 - (needed.value.length * 100 / allStickers.value.length)));
+
+const copyToClipboard = (stickers: Sticker[]) => {
+  navigator.clipboard.writeText(stickers.map(sticker => sticker.code).join(", "));
+}
 </script>
 
 <template>
@@ -24,30 +30,31 @@ const completionPercentage = computed(() => Math.floor(100 - (needed.value.lengt
         </ul>
       </template>
     </Heading>
-    <section class="grid grid-cols-2 gap-8 mb-8">
+    <section class="grid grid-cols-1 sm:grid-cols-2 gap-4 md:gap-8 mb-8">
       <div class="card card-normal bg-base-100 overflow-visible">
         <div class="card-body">
-          <h2 class="card-title flex items-center">Repetidas <span class="badge">{{spares.length}}</span></h2>
-          <ul v-if="spares.length > 0" class="grid grid-cols-6 gap-4">
-            <li v-for="sticker in spares" class="tooltip tooltip-top" :data-tip="`${sticker.name} - ${sticker.spareAmount}x`">
-              <button class="btn w-full">{{sticker.code}}</button>
-            </li>
-          </ul>
+          <div class="w-full flex items-center justify-between mb-4">
+            <h2 class="prose-lg font-semibold flex items-center gap-2">Repetidas <span class="badge">{{spares.length}}</span></h2>
+            <button class="btn btn-secondary tooltip tooltip-top" data-tip="Copiar lista" @click="() => copyToClipboard(spares)">
+              <v-icon name="ri-file-copy-line"></v-icon>
+            </button>
+          </div>
+          <CodeGrid v-if="spares.length > 0" :stickers="spares"/>
+          <p v-else>Nenhuma figurinha repetida</p>
         </div>
       </div>
       <div class="card card-normal bg-base-100 overflow-visible">
         <div class="card-body">
-          <div class="w-full flex justify-between">
-            <h2 class="card-title flex items-center gap-2 m-0">
+          <div class="w-full flex items-center justify-between mb-4">
+            <h2 class="prose-lg font-semibold flex items-center gap-2 tooltip tooltip-top" :data-tip="`${completionPercentage}% do álbum concluído`">
               Faltantes <span class="badge">{{needed.length}}</span>
             </h2>
-            <p class="text-right text-sm">{{completionPercentage}}% do álbum concluído</p>
+            <button class="btn btn-secondary tooltip tooltip-top" data-tip="Copiar lista" @click="() => copyToClipboard(needed)">
+              <v-icon name="ri-file-copy-line"></v-icon>
+            </button>
           </div>
-          <ul v-if="needed.length > 0" class="grid grid-cols-6 gap-4">
-            <li v-for="sticker in needed" class="tooltip tooltip-top" :data-tip="sticker.name">
-              <button class="btn w-full">{{sticker.code}}</button>
-            </li>
-          </ul>
+          <CodeGrid v-if="needed.length > 0" :stickers="needed"></CodeGrid>
+          <p v-else>Não está precisando de nenhuma figurinha</p>
         </div>
       </div>
     </section>
