200f178b7115fbb0bce2dda46d5eed4dd6847224
Leonardo Oliveira <dev@leooliveira.me>
2022-09-06 11:27:39 -0300

    Tweaks on home page

--- src/components/CodeGrid.vue
+++ src/components/CodeGrid.vue
@@ -5,7 +5,7 @@ defineProps<{
 </script>
 
 <template>
-  <ul class="grid grid-cols-4 lg:grid-cols-6 gap-4">
+  <ul class="grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-6 gap-2 sm:gap-4">
     <li v-for="sticker in stickers" class="tooltip tooltip-top" :data-tip="sticker.name">
       <button class="btn w-full">{{sticker.code}}</button>
     </li>
