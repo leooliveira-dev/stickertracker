c3b685532a38ab0a14799276e7537270bba74adf
Leonardo Oliveira <dev@leooliveira.me>
2022-09-18 16:32:37 -0300

    Improve spare tracking

--- src/components/CollectionItem.vue
+++ src/components/CollectionItem.vue
@@ -20,17 +20,21 @@ watch(
   { immediate: true }
 );
 
-const handleSpareInput = (event: Event) => {
-  counter.value = Number(getInputEventValue(event));
+const addSpare = () => {
+  counter.value++;
   emits("update:spareAmount", counter.value);
-};
+}
+const subtractSpare = () => {
+  counter.value--;
+  emits("update:spareAmount", counter.value);
+}
 </script>
 <template>
   <div
     class="card card-compact mb-2 overflow-hidden"
     :class="sticker.marked ? 'bg-primary' : 'bg-base-100'"
   >
-    <div class="card-body grid grid-cols-3 items-center overflow-hidden">
+    <div class="card-body grid grid-cols-3 items-center">
       <div v-if="isFoil" class="foil absolute left-0 h-full w-32"></div>
       <div class="relative flex flex-row items-center gap-2">
         <Flag :country="sticker.country ?? ''"></Flag>
@@ -38,7 +42,7 @@ const handleSpareInput = (event: Event) => {
       </div>
       <p class="hidden sm:block justify-self-center">{{ sticker.name }}</p>
       <ul class="flex flex-row items-center gap-4 justify-self-end">
-        <li class="tooltip tooltip-top" data-tip="Tenho">
+        <li class="tooltip tooltip-left" data-tip="Tenho">
           <button
             class="btn btn-circle"
             :class="!sticker.marked ? '' : 'btn-success'"
@@ -47,23 +51,22 @@ const handleSpareInput = (event: Event) => {
             <v-icon :name="sticker.marked ? 'ri-check-fill' : 'ri-add-fill'" />
           </button>
         </li>
-        <li class="tooltip tooltip-top" data-tip="Repetida">
+        <li class="tooltip tooltip-left" data-tip="Repetida">
           <button
             class="btn btn-circle"
-            :class="!sticker.marked ? 'btn-disabled' : 'btn-warning'"
+            :class="!sticker.marked ? 'btn-disabled' : 'btn-error'"
             :disabled="!sticker.marked"
             @click="emits('update:spareAmount', counter + 1)"
           >
             <v-icon name="ri-file-copy-line" />
           </button>
         </li>
-        <li v-if="counter > 0">
-          <input
-            type="number"
-            :value="counter"
-            @input="(event) => handleSpareInput(event)"
-            class="bg-base-300 w-24 border-none rounded-md"
-          />
+        <li v-if="counter > 0" class="tooltip tooltip-left" data-tip="Número de repetidas">
+          <span class="font-bold text-xl text-white">{{counter}}x</span>
+        </li>
+        <li v-if="counter > 0" class="flex flex-col items-center w-full gap-1">
+          <button @click.stop="addSpare" class="bg-base-300 font-bold text-white rounded-md py-1 px-2 w-full">+</button>
+          <button @click.stop="subtractSpare" class="bg-base-300 font-bold text-white rounded-md py-1 px-2 w-full">-</button>
         </li>
       </ul>
     </div>
